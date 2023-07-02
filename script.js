655be6e20762eea32652d4b8ff30aa7c7f924c83
Avery Miller <averymiller2060@gmail.com>
2023-07-01 23:23:50 -0400

    todo items are loaded on page load and are effectively removed when user clicks the trash icon

--- script.js
+++ script.js
@@ -1,38 +1,71 @@
-document.addEventListener("DOMContentLoaded", () => {
 
-  const form = document.querySelector("[data-form]");
-  const lists = document.querySelector("[data-lists]");
-  const input = document.querySelector("[data-input]");
+const form = document.querySelector("[data-form]");
+const lists = document.querySelector("[data-lists]");
+const input = document.querySelector("[data-input]");
 
-  let todoArr = [];
-
-  form.addEventListener("submit", (e) => {
-    e.preventDefault();
-    let id = Math.ceil(Math.random() * 10000000);
-    const todo = new Todo(id, input.value);
-    todoArr = [...todoArr, todo];
-    UI.displayData();
-  });
-
-  class Todo {
-    constructor(id, todo) {
-      this.id = id;
-      this.todo = todo;
-    }
+class Storage {
+  static addToStorage(todoArr) {
+    let storage = localStorage.setItem("todo", JSON.stringify(todoArr));
+    return storage;
   }
-
-  class UI {
-    static displayData() {
-      let displayData = todoArr.map((item) => {
-        return `
-          <div class="todo">
-            <p>${item.todo}</p>
-            <span>🗑️</span>
-          </div>
-        `
-      });
-      lists.innerHTML = displayData.join(" ");
-      console.log(displayData);
-    }
+  static getStorage() {
+    let storage = localStorage.getItem("todo") === null ?
+    [] : JSON.parse(localStorage.getItem("todo"));
+    return storage;
   }
+}
+
+let todoArr = Storage.getStorage();
+
+form.addEventListener("submit", (e) => {
+  e.preventDefault();
+  let id = Math.ceil(Math.random() * 10000000);
+  const todo = new Todo(id, input.value);
+  todoArr = [...todoArr, todo];
+  UI.displayData();
+  UI.clearInput();
+  UI.removeTodo();
+  Storage.addToStorage(todoArr);
+});
+
+class Todo {
+  constructor(id, todo) {
+    this.id = id;
+    this.todo = todo;
+  }
+}
+
+class UI {
+  static displayData() {
+    let displayData = todoArr.map((item) => {
+      return `
+        <div class="todo">
+          <p>${item.todo}</p>
+          <span class="remove" data-id=${item.id}>🗑️</span>
+        </div>
+      `
+    });
+    lists.innerHTML = displayData.join(" ");
+  }
+  static clearInput() {
+    input.value = "";
+  }
+  static removeTodo() {
+    lists.addEventListener("click", (e) => {
+      if(e.target.classList.contains("remove")) {
+        e.target.parentElement.remove();
+      }
+      let btnID = e.target.dataset.id;
+      UI.removeArrayTodo(btnID);
+    });
+  }
+  static removeArrayTodo(id) {
+    todoArr = todoArr.filter((item) => item.id !== +id);
+    Storage.addToStorage(todoArr);
+  }
+}
+
+window.addEventListener("DOMContentLoaded", () => {
+  UI.displayData();
+  UI.removeTodo();
 });
